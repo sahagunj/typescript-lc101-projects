5996db20a146ff0e9373a144bdc74790a47717c8
Jessica Sahagun <jessicasahagun@Jessicas-Air.lan>
2022-03-13 16:14:47 -0500

    initial commit

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,20 @@
 // Paste in the provided code here:
+export class SpaceLocation {
+    kilometersAway: number;
+    name: string;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+    
+    getDaysToLocation(kilometersAway: number): number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hours: number = milesAway / this.speedMph;
+        return hours / 24;
+     }
+
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+ }
