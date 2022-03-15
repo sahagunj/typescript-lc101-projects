9b2acfc0fc6144a81da985ff89d47d2958c1003b
Jessica Sahagun <jessicasahagun@Jessicas-Air.lan>
2022-03-14 19:47:29 -0500

    initial commit

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { IterationStatement } from "typescript";
+import { Cargo } from './Cargo';
+import { Payload } from './Payload';
+import { Astronaut } from './Astronaut';
+
+export class Rocket {
+    // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass( items: Payload[] ): number{
+        let sum:number=0;
+        for (let index = 0; index < items.length; index++) {
+            sum += items[index].massKg;
+        }
+        return sum;
+    }
+
+    
+    currentMassKg(): number{
+        return this.sumMass(this.astronauts)+this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean{
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo): boolean{
+        if(this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean{
+        if(this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+
+}
